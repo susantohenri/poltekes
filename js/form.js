2ef8c275cf1868e506a678d601df57c5e36a8c23
Henri Susanto <henry.dinus@gmail.com>
2018-12-03 03:36:58 +0700

    bugfix subform

--- js/form.js
+++ js/form.js
@@ -13,31 +13,29 @@ window.onload = function () {
     var fchild = $(this)
     var controller = site_url + fchild.attr('data-controller')
     var uuids = JSON.parse(fchild.attr('data-uuids').split("'").join('"'))
-    var requests = []
-    for (var u in uuids) requests.push($.ajax({url: controller + '/subformread/' + uuids[u], success: function (form) {
+    for (var u in uuids) $.ajax({url: controller + '/subformread/' + uuids[u], success: function (form) {
       fchild.prepend(form)
-      formInit()
-    }}))
-    $.when.apply(undefined, requests).then(function () {
-      var elements = $('.form-child .row')
-      var elems = []
-      for( var i = 0; i < elements.length; ++i ) {
-        var el = elements[i]
-        elems.push(el)
+      if (uuids.length === fchild.find('[data-urutan]').length) {
+        var elements = fchild.find('[data-urutan]')
+        var elems = []
+        for( var i = 0; i < elements.length; ++i ) {
+          var el = elements[i]
+          elems.push(el)
+        }
+        var sorted = elems.sort(function (a, b) {
+          var aValue = parseInt(a.getAttribute('data-urutan'), 10)
+          var bValue = parseInt(b.getAttribute('data-urutan'), 10)
+          return aValue - bValue
+        })
+        var html = ''
+        elements.remove()
+        for( var i = 0; i < sorted.length; ++i ) html += sorted[i].outerHTML
+        fchild.prepend(html)
+        formInit()
       }
-      var sorted = elems.sort(function (a, b) {
-        var aValue = parseInt(a.getAttribute('data-urutan'), 10)
-        var bValue = parseInt(b.getAttribute('data-urutan'), 10)
-        return aValue - bValue
-      })
-
-      var html = ''
-      elements.remove()
-      for( var i = 0; i < sorted.length; ++i ) html += sorted[i].outerHTML
-      $(html).insertBefore($('.btn.btn-warning.btn-sm.btn-add'))
-    })
+    }})
     fchild.find('.btn-add').click(function () {
-        var beforeButton = $(this).parents('.form-group');
+      var beforeButton = $(this).parents('.form-group');
       $.get(controller + '/subformcreate/', function (form) {
         $(form).insertBefore(beforeButton)
         formInit()
