b479375f5e1257147f046094c39db882def6268f
Henri Susanto <henry.dinus@gmail.com>
2019-01-24 06:33:02 +0700

    entity assignment

---
+++ application/migrations/017_assignment.php
@@ -0,0 +1,24 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Migration_assignment extends CI_Migration {
+
+  function up () {
+
+    $this->db->query("
+      CREATE TABLE `assignment` (
+        `uuid` varchar(255) NOT NULL,
+        `jabatanGroup` varchar(255) NOT NULL,
+        `detail` varchar(255) NOT NULL,
+        `urutan` INT(11) UNIQUE NOT NULL AUTO_INCREMENT ,
+        PRIMARY KEY (`uuid`)
+      ) ENGINE=InnoDB DEFAULT CHARSET=utf8
+    ");
+
+  }
+
+  function down () {
+    $this->db->query("DROP TABLE IF EXISTS `assignment`");
+  }
+
+}
